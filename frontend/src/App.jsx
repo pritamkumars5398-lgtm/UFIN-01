4b7537254e2d9b507a76336117365ab9ba88faf0
student app <kumariaimran2018@gmail.com>
2026-06-22 16:07:44 +0530

    fix missing LanguageProvider import in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
+import { LanguageProvider } from './context/LanguageContext'
 
 export default function App() {
   return (
